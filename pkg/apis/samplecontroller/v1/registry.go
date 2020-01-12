96813f2c02c003f56301c7394d3d51bfefe67e5b
root <root@master.lj.io>
2020-01-12 14:40:21 +0800

    update

--- pkg/apis/samplecontroller/v1/registry.go
+++ pkg/apis/samplecontroller/v1/registry.go
@@ -7,7 +7,7 @@ import (
 )
 
 // SchemeGroupVersion is group version used to register these objects
-var SchemeGroupVersion = schema.GroupVersion{Group: "nicktming.example.com", Version: "v1"}
+var SchemeGroupVersion = schema.GroupVersion{Group: "samplecontroller.lj.io", Version: "v1"}
 
 // Kind takes an unqualified kind and returns back a Group qualified GroupKind
 func Kind(kind string) schema.GroupKind {
